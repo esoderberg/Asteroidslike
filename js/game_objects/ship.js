08e877982fef87e80b13b8cfcdb552edd9d91e09
Erik Söderberg <erikvsoderberg@gmail.com>
2019-08-09 01:06:54 +0200

    Add jsdoc

--- js/game_objects/ship.js
+++ js/game_objects/ship.js
@@ -1,9 +1,19 @@
 /*jshint esversion: 6 */
 import Entity from "./entity.js";
 import {StandardGun, TriGun} from "./gunModule.js";
+import { Physics } from "phaser";
 
 export class Ship extends Entity{
-    constructor({sargs, world,  inputKeys:{forward, left, right, fire}}){
+    /**
+     * @param {Object} config
+     * @param {*} config.sargs - Arguments to Physics.Sprite
+     * @param {Object.<string, Phaser.Input.Keyboard.Key} config.inputKeys - Keyboard keys map
+     * @param {Phaser.Input.Keyboard.Key} config.inputKeys.forward - Move forward
+     * @param {Phaser.Input.Keyboard.Key} config.inputKeys.left - Rotate ship left.
+     * @param {Phaser.Input.Keyboard.Key} config.inputKeys.right - Rotate ship right.
+     * @param {Phaser.Input.Keyboard.Key} config.inputKeys.fire - Fire ship gun.
+     */
+    constructor({sargs, inputKeys:{forward, left, right, fire}}){
         super(sargs);
         
         this.setDrag(0.99);
@@ -12,7 +22,6 @@ export class Ship extends Entity{
         this.score = 0;
         this.lives = 3;
         this.scene.registry.set('playerLives', this.lives);
-        this.world = world;
         // Bindings for input
         this.forward = forward;
         this.left = left;
@@ -77,21 +86,38 @@ export class Ship extends Entity{
         }
     }
 
+    /**
+     * Reloads the gun on this ship.
+     */
     reload(){
         this.gunModule.reload();
     }
 
+    /**
+     * @returns {number} the rotation from +x-axis that would point a vector forward.
+     */
     get rotForward() { return this.rotation-Math.PI/2;}
 
+    /**
+     * @returns {Phaser.Math.Vector2} Vector that points in the same direction as the ship.
+     */
     facing(){
         return new Phaser.Math.Vector2(Math.sin(this.rotation), -Math.cos(this.rotation));
     }
 
+    /**
+     * 
+     * @param {number} score - the score to add.
+     */
     addScore(score){
         this.score += score;
         this.scene.registry.set('score', this.score);
     }
 
+    /**
+     * Kills the ship.
+     * This subtracts a life and makes the ship invisible.
+     */
     kill(){
         this.vulnerable = false;
         this.lives -= 1;
@@ -101,8 +127,17 @@ export class Ship extends Entity{
         this.setActive(false).setVisible(false);
     }
 
+    /**
+     * @returns true if the ship can be damaged.
+     */
     isVulnerable(){ return this.vulnerable; }
 
+    /**
+     * Makes the ship invulnerable for the specified duration. 
+     * Whilst invulnerable the ship blinks.
+     * 
+     * @param {number} seconds the duration to make the ship invulerable.
+     */
     makeTempInvulnerable(seconds){
         this.vulnerable = false;
         let blinkEvent = this.scene.time.addEvent({delay:200, callback: () => {this.setVisible(!this.visible);}, callbackScope: this, repeat:-1});
@@ -113,6 +148,12 @@ export class Ship extends Entity{
                 this.setVisible(true);}, null, this);
     }
 
+    /**
+     * Respawns the ship and the coordinates provided and makes it invulnerable for 3 seconds.
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
     respawn(x,y){
         this.setActive(true).setVisible(true);
         this.setRotation(0);
